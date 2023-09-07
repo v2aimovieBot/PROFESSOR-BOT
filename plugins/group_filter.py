b091d71e7368cbbc765ccf1d62ec52a4674dc1b7
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2023-09-07 17:04:01 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -1,4 +1,4 @@
-8import asyncio, re, ast, math, logging
+import asyncio, re, ast, math, logging
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 from utils import get_shortlink, replace_username 
